c75b2e006de9925799a49ce6f2190b22d90ddecb
valentim <valentin.minkovich@amdocs.com>
2023-04-05 14:01:09 +0300

    #99 - Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,6 +34,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numOfTours = await Tour.countDocuments();
+      if (skip >= numOfTours) throw new Error('This page does not exist.');
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
 
