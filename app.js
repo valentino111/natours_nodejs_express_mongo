fa4afbc174bce6ab50829a633acd6b3e0d2f4ae5
valentim <valentin.minkovich@amdocs.com>
2023-04-07 17:13:51 +0300

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -20,15 +20,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
 // 3) ROUTES
-
 app.use('/api/v1/tours', tourRouter);
-
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 module.exports = app;
