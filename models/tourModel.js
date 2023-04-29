e42068d2196e06390591467381ae0318ce55a03c
valentim <valentin.minkovich@amdocs.com>
2023-04-29 15:32:41 +0300

    151 - Modeling Tour Guides: Child Refferencing.

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const slugify = require('slugify');
 
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const validator = require('validator');
 
@@ -109,7 +109,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -127,12 +132,12 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 
-  next();
-});
+//   next();
+// });
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document...');
