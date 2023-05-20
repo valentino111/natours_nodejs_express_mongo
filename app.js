eec98b3f6fa50a2e85a9039b1a500de586d2068a
Valentin Minkovich <valentino111@gmail.com>
2023-05-20 19:07:09 +0300

    181 - Setting up the Project Structure.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRouts');
 const userRouter = require('./routes/userRouts');
 const reviewRouter = require('./routes/reviewRouts');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = expess();
 
@@ -71,26 +72,8 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-    title: 'Exciting tours for adventurous people',
-  });
-});
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
