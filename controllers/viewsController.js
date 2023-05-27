b8bd32e12269a31b2034530a4115203f0307393c
Valentin Minkovich <valentino111@gmail.com>
2023-05-27 16:42:40 +0300

    184 - Building the Tour Page - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,18 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // 2) Build template
+
+  // 3) Render templateusing data from 1)
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
