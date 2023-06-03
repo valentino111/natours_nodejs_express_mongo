db183afedf238c5b4305cbc3db02447738e691d5
Valentin Minkovich <valentino111@gmail.com>
2023-06-03 18:37:33 +0300

    192 - logging out users.

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,6 +68,14 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -112,38 +120,42 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // Only for rendered pages, no error
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 1) Verification of the token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET
-    );
-    console.log('Decoded: ', decoded);
-
-    // 2) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    console.log(currentUser);
-    if (!currentUser) {
-      return next(
-        new AppError(
-          'The user belonging to this token is no longer exists.',
-          401
-        )
+    try {
+      // 1) Verification of the token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
       );
-    }
+      console.log('Decoded: ', decoded);
 
-    // 3) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      console.log(currentUser);
+      if (!currentUser) {
+        return next(
+          new AppError(
+            'The user belonging to this token is no longer exists.',
+            401
+          )
+        );
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE'S A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
       return next();
     }
-
-    // THERE'S A LOGGED IN USER
-    res.locals.user = currentUser;
-    return next();
   }
   next();
-});
+};
 
 exports.restrictTo =
   (...roles) =>
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 // Delegation
 if (mapBox) {
@@ -21,4 +22,8 @@ if (loginForm) {
 
     login(email, password);
   });
+}
+
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logout);
 }
