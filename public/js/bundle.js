956552f306e90c10b9a9d1628515b4ac12667ac7
Valentin Minkovich <valentino111@gmail.com>
2023-06-05 21:07:34 +0300

    195 - Updating User Data.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12275,7 +12275,7 @@ var _login = require("./login");
 
 // DOM elements
 var mapBox = document.getElementById('map');
-var loginForm = document.querySelector('.form');
+var loginForm = document.querySelector('.form--login');
 var logOutBtn = document.querySelector('.nav__el--logout');
 
 // Delegation
