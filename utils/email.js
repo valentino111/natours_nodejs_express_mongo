556304ad12ade85d8964a37af29e7a704c4dd759
Valentin Minkovich <valentino111@gmail.com>
2023-06-22 19:17:17 +0300

    206 - Building a Complex Email Handler.

--- utils/email.js
+++ utils/email.js
@@ -1,29 +1,63 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    // service: 'Gmail', //for gmail
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    // Activate in gmail "less secure app" option //for gmail
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Valentin Minkovich <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Valentin Minkovidh <valentino111@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
 
-  // 3) Send email with nodemaier
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      // service: 'Gmail', //for gmail
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      // Activate in gmail "less secure app" option //for gmail
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../veiws/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.fromString(html),
+      // html:
+    };
+
+    // 3) Create transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Naturs family!');
+  }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async (options) => {
+//   // 3) Send email with nodemaier
+//   await transporter.sendMail(mailOptions);
+// };
