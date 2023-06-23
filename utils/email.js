3369bfe3d55260b18ea24edff5b050338693aa62
Valentin Minkovich <valentino111@gmail.com>
2023-06-23 17:00:39 +0300

    208 - Sending Password Reset Emails.

--- utils/email.js
+++ utils/email.js
@@ -58,6 +58,13 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Naturs family!');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes.)'
+    );
+  }
 };
 
 // const sendEmail = async (options) => {
