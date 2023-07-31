f725e90fea9b7d31d2b2f49566df4e1802fc4ec1
Valentin Minkovich <valentino111@gmail.com>
2023-07-31 15:44:31 +0300

    Enlarge fonst for QUICK FACTS and YOUR TOUR GUIDES.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12968,7 +12968,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58238" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57642" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
